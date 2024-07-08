b079a4b3e820acc9f3eba6848af387de06005395
DHan0913 <163090296+DHan0913@users.noreply.github.com>
2024-07-08 16:37:16 +0900

    Delete dvd directory

--- dvd/src/main/java/himedia/dvd/repositories/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package himedia.dvd.repositories.dao;
-
-import himedia.dvd.repositories.vo.UserVo;
-
-public interface UserDao {
-    int insert(UserVo user);
-    UserVo selectUserByEmail(String email);
-    UserVo selectUserByEmailAndPassword(String email, String password);
-}
