5b3a8f829b0c5f6ba994453f2d7296d0cbed148c
Yesssung <cjsrnr0504@naver.com>
2024-07-08 16:52:55 +0900

    24.07.08 학원

--- src/main/java/himedia/dvd/services/UpdateService.java
+++ src/main/java/himedia/dvd/services/UpdateService.java
@@ -1,7 +1,10 @@
 package himedia.dvd.services;
 
-public interface UpdateService{
-	public boolean update(UpdateVo vo) {
-		
-	}
+import himedia.dvd.repositories.vo.UserVo;
+
+public interface UpdateService {
+	public boolean update(UserVo vo);
+	public UserVo getUser(String email);
+	public UserVo getUser(String emial, String birth);
+	
 }
