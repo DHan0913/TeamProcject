366695d0401129b73d732a2f1b3f32b00a379a1e
DHan0913 <eogml01023@gmail.com>
2024-07-12 17:08:15 +0900

    각종 더미 데이터 통합

--- src/main/java/himedia/dvd/repositories/dao/UserDao.java
+++ src/main/java/himedia/dvd/repositories/dao/UserDao.java
@@ -6,15 +6,16 @@ import himedia.dvd.repositories.vo.UserVo;
 
 public interface UserDao {
 	List<UserVo> selectAllUsers();
-    int insert(UserVo user);	//	회원 가입
-    UserVo selectUserByEmail(String email);	//	중복 이메일	
-    UserVo selectUserByEmailAndPassword(String email, String password);	//	로그인용
-    
-    boolean update(UserVo user);	// 회원정보수정
 
-    boolean delete(UserVo user);	// 회원정보삭제
-    
-    boolean delete(Long userNo);	//	유저 번호로 삭제
-    UserVo selectUserByUserNo(Long userNo);    // 유저 번호로 사용자 조회
+	int insert(UserVo user); // 회원 가입
+
+	UserVo selectUserByEmail(String email); // 중복 이메일
+
+	UserVo selectUserByEmailAndPassword(String email, String password); // 로그인용
+
+	boolean update(UserVo user); // 회원정보수정
+
+	boolean delete(Long userNo); // 유저 번호로 삭제
+
 
 }
